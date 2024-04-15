5b988ea0f9b515b8937598214d6b7e0f17d8290f
kaminoontee <robotrd2015@gmail.com>
2024-04-15 17:09:27 +0300

    add all validations

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import socket
+import struct
+
+
+def crc16_teltonika(pData):
+    crc16_result = 0x0000
+    for i in range(len(pData)):
+        val = pData[i]
+        crc16_result ^= val
+        for j in range(8):
+            if crc16_result & 0x0001:
+                crc16_result = (crc16_result >> 1) ^ 0xA001
+            else:
+                crc16_result >>= 1
+    return crc16_result
 
 
 def main():
@@ -17,18 +31,47 @@ def main():
         # print(length)
 
         if rest_data == b'866897050116377' and len(rest_data) == length:
-            conn.send()
+            response = 1
+            conn.send(response.to_bytes(1, byteorder='big'))
+            data_full = conn.recv(1024)
+            # raw_data = b'\x00\x00\x00\x00\x00\x00\x006\x08\x01\x00\x00\x01k@\xd8\xea0\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x05\x02\x15\x03\x01\x01\x01B^\x0f\x01\xf1\x00\x00`\x1a\x01N\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\xc7\xcf'
+            items = [int(item) for item in data_full]
+            avl_data_raw = data_full[8:-4]
+            if ((all(items[:4]) is not False) or (
+                    crc16_teltonika(avl_data_raw) != int(str(crc_sum_raw.hex()), base=16)) or
+                    (len(avl_data_raw) != int(str(avl_data_len.hex()), base=16))):
+                break
+            print(data_full)
 
     conn.close()
+
+
 #     b'\x00\x0f866897050116377'
+# b'\x00\x00\x00\x00\x00\x00\x006\x08\x01\x00\x00\x01k@\xd8\xea0\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x05\x02\x15\x03\x01\x01\x01B^\x0f\x01\xf1\x00\x00`\x1a\x01N\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\xc7\xcf'
 
 
 if __name__ == '__main__':
-    main()
-    # data = b'\x00\x0f866897050116377'
-    # enc_1, enc_2 = data[:2]
-    # rest_data = data[2:]
-    # print(enc_1)
-    # print(enc_2)
-    # print(rest_data)
-    # print(rest_data == data[2:])
+    # main()
+    raw_data = b'\x00\x00\x00\x00\x00\x00\x006\x08\x01\x00\x00\x01k@\xd8\xea0\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x05\x02\x15\x03\x01\x01\x01B^\x0f\x01\xf1\x00\x00`\x1a\x01N\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\xc7\xcf'
+    items = [int(item) for item in raw_data]
+    crc_sum = items[-4:]
+    crc_sum_raw = raw_data[-4:]
+    avl_data = items[10:-5]
+    avl_data_raw = raw_data[8:-4]
+    avl_data_len = raw_data[4:8]
+    print(avl_data_len)
+    records_count = items[9]
+
+    print(all(items[:4]) is False)
+    print(items)
+
+    print(crc16_teltonika(avl_data_raw))
+    print(int(str(crc_sum_raw.hex()), base=16))
+    print(int(str(avl_data_len.hex()), base=16))
+
+    date_raw = avl_data_raw[2:8]
+    # rdate_raw = date_raw[::-1]
+    # print(rdate_raw)
+    # print(date_raw.decode())
+    # date = avl_data_raw[]
+    # print(date)
